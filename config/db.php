bf1fbf521db896ea6cc23a449fc5a02b90f6c8e4
apipem <andres.mayorga15@gmail.com>
2024-04-08 11:03:48 -0500

    db

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=clasroom',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306;dbname=nf87lb8wczqe92ry',
+    'username' => 'ckerkjmrz0jq3qxy',
+    'password' => 'z4jg31jv4cjoy1ro',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
