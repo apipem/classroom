68d62573a8950a5e26defa846884e3d6f8960084
apipem <andres.mayorga15@gmail.com>
2024-04-10 15:31:08 -0500

    Update

--- Documentacion/clasroom.sql
+++ Documentacion/clasroom.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 08-04-2024 a las 05:36:39
+-- Tiempo de generación: 10-04-2024 a las 20:28:59
 -- Versión del servidor: 5.7.33
 -- Versión de PHP: 7.4.19
 
@@ -16,6 +16,12 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `clasroom`
+--
+CREATE DATABASE IF NOT EXISTS `clasroom` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `clasroom`;
 
 -- --------------------------------------------------------
 
@@ -105,7 +111,8 @@ CREATE TABLE `contenido` (
   `contenido` text NOT NULL,
   `descripcion` text NOT NULL,
   `materia` int(11) NOT NULL,
-  `proyecto` int(11) NOT NULL
+  `proyecto` int(11) NOT NULL,
+  `user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -113,15 +120,6 @@ CREATE TABLE `contenido` (
 --
 
 TRUNCATE TABLE `contenido`;
---
--- Volcado de datos para la tabla `contenido`
---
-
-INSERT INTO `contenido` (`idcontenido`, `contenido`, `descripcion`, `materia`, `proyecto`) VALUES
-(1, 'recurso/pr.html', '1', 17, 19),
-(3, 'recurso/1-b.jpg', '1', 22, 19),
-(4, 'recurso/2-b.jpg', '2', 21, 11);
-
 -- --------------------------------------------------------
 
 --
@@ -144,19 +142,6 @@ CREATE TABLE `curso` (
 --
 
 TRUNCATE TABLE `curso`;
---
--- Volcado de datos para la tabla `curso`
---
-
-INSERT INTO `curso` (`idcurso`, `estudiante`, `materia`, `profesor`, `nota`, `notificacion`, `notas`) VALUES
-(11, 29, 17, 20, 0, NULL, 11),
-(13, 29, 21, 24, 0, NULL, 13),
-(14, 29, 22, 24, 0, NULL, 14),
-(15, 23, 20, 20, 0, NULL, 15),
-(16, 23, 21, 24, 0, NULL, 16),
-(17, 25, 20, 20, 0, NULL, 17),
-(18, 25, 21, 24, 0, NULL, 18);
-
 -- --------------------------------------------------------
 
 --
@@ -178,16 +163,6 @@ CREATE TABLE `materia` (
 --
 
 TRUNCATE TABLE `materia`;
---
--- Volcado de datos para la tabla `materia`
---
-
-INSERT INTO `materia` (`idmateria`, `nombre`, `codigo`, `vcorte1`, `vcorte2`, `vcorte3`) VALUES
-(17, 'Psicologia', 'ASCV56', '40', '40', '20'),
-(20, 'ingles', 'AB369', '20', '20', '60'),
-(21, 'Criminalistica', '258FSJ', '30', '30', '40'),
-(22, 'pg1', 'BSFG25', '0', '50', '50');
-
 -- --------------------------------------------------------
 
 --
@@ -208,20 +183,6 @@ CREATE TABLE `notas` (
 --
 
 TRUNCATE TABLE `notas`;
---
--- Volcado de datos para la tabla `notas`
---
-
-INSERT INTO `notas` (`idnotas`, `corte1`, `corte2`, `corte3`, `proyecto`) VALUES
-(11, 0, 0, 0, 19),
-(12, 4, 4, 3, 19),
-(13, 0, 0, 0, 19),
-(14, 0, 0, 0, 19),
-(15, 0, 0, 0, 19),
-(16, 0, 0, 0, 19),
-(17, 0, 0, 0, 17),
-(18, 0, 0, 0, 17);
-
 -- --------------------------------------------------------
 
 --
@@ -242,18 +203,6 @@ CREATE TABLE `proyecto` (
 --
 
 TRUNCATE TABLE `proyecto`;
---
--- Volcado de datos para la tabla `proyecto`
---
-
-INSERT INTO `proyecto` (`idProyecto`, `nombre`, `descripcion`, `fechaIncio`, `fechaFin`) VALUES
-(8, 'Proyecto3', 'Descripcion Proyecto3', '2024-03-05', '2024-03-05'),
-(11, 'Sennova', 'Proyecto con convenio', '2024-02-02', '2024-02-20'),
-(16, 'p1', 'p1', '2024-04-17', '2024-04-18'),
-(17, 'p2', 'p2', '2024-04-08', '2024-04-09'),
-(18, 'p3', 'p3', '2024-04-02', '2024-04-03'),
-(19, 'ClassRoom', 'Proyecto interno', '2024-04-01', '2024-04-02');
-
 -- --------------------------------------------------------
 
 --
@@ -282,16 +231,7 @@ TRUNCATE TABLE `usuario`;
 --
 
 INSERT INTO `usuario` (`idUsuario`, `cc`, `nombre`, `apellido`, `correo`, `contrasena`, `rol`, `estado`) VALUES
-(2, 1052, 'andres', 'mayo', 'amayorga3@udi.edu.co', '$2y$10$waK2McV.wV99XJ53QA5gru.EFN53lreUL/YfrSsKDDJ6xnBVeWPia', 'estudiante', 1),
-(20, 1, 'Luisa', 'Mendez', 'lm@g', '$2y$13$EVr.fOGex3buRWbmoKCoYeqkoapW/upex8Blk.Xt6V3TNsBGmbEKm', 'profesor', 1),
-(21, 2, 'Estiven', 'Cigal', 'EC@g', '$2y$13$.lFHKo7BupgzM4/L9OP3kuM2BqbQdOvKILUWT/1Rozw/EO/dMOxq6', 'estudiante', 1),
-(22, 3, 'Yadis', 'levil', 'yv@', '$2y$13$eMKwm0mWSSdxEkC6wNsaOOt3BkRnm0xvV0bjzPfKOe7KoAk8sod8.', 'estudiante', 1),
-(23, 4, 'James', 'Gomez', 'jg@', '$2y$10$waK2McV.wV99XJ53QA5gru.EFN53lreUL/YfrSsKDDJ6xnBVeWPia', 'estudiante', 1),
-(24, 5, 'Leila', 'Graciela', 'lg@', '$2y$13$e4E/kPwmSEGpwNuauSjw.uiG79tWEpLI0lSxmE49mkH7/OWZ3xymm', 'profesor', 1),
-(25, 6, 'Camila', 'Vargas', 'cm@', '$2y$10$waK2McV.wV99XJ53QA5gru.EFN53lreUL/YfrSsKDDJ6xnBVeWPia', 'estudiante', 1),
-(28, 11, 'once', 'once apellido', '11', '$2y$13$EbQVukEuyCDBZXWBRWdsteaq/JRStBx5DXR.KIkEnMHA.0WtwO07G', 'estudiante', 1),
-(29, 20, 'Darwen', 'Zenmed', '20', '$2y$13$ErojvfLjNykZZpRAIkoo4eYfVUdYHxxOgdwG9wy.TtfD4cbRBEjwy', 'estudiante', 1),
-(30, 1052415816, 'a', 'v', '2', '$2y$13$S8XjAg6CIq10mbXP9gL3jun1gXD/NPar1GDAr1qvAZmZyKnmLQ60G', 'profesor', 1);
+(2, 1052, 'admin', 'admin', 'admin', '$2y$10$waK2McV.wV99XJ53QA5gru.EFN53lreUL/YfrSsKDDJ6xnBVeWPia', 'estudiante', 0);
 
 --
 -- Índices para tablas volcadas
@@ -331,7 +271,8 @@ ALTER TABLE `auth_rule`
 ALTER TABLE `contenido`
   ADD PRIMARY KEY (`idcontenido`),
   ADD KEY `fk_contenido_materia1_idx` (`materia`),
-  ADD KEY `fk_contenido_Proyecto1_idx` (`proyecto`);
+  ADD KEY `fk_contenido_Proyecto1_idx` (`proyecto`),
+  ADD KEY `fk_contenido_usuario1_idx` (`user`);
 
 --
 -- Indices de la tabla `curso`
@@ -376,37 +317,37 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `contenido`
 --
 ALTER TABLE `contenido`
-  MODIFY `idcontenido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `idcontenido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT de la tabla `curso`
 --
 ALTER TABLE `curso`
-  MODIFY `idcurso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `idcurso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de la tabla `materia`
 --
 ALTER TABLE `materia`
-  MODIFY `idmateria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `idmateria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT de la tabla `notas`
 --
 ALTER TABLE `notas`
-  MODIFY `idnotas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `idnotas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT de la tabla `proyecto`
 --
 ALTER TABLE `proyecto`
-  MODIFY `idProyecto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `idProyecto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- Restricciones para tablas volcadas
@@ -417,14 +358,15 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `contenido`
   ADD CONSTRAINT `fk_contenido_Proyecto1` FOREIGN KEY (`proyecto`) REFERENCES `proyecto` (`idProyecto`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_contenido_materia1` FOREIGN KEY (`materia`) REFERENCES `materia` (`idmateria`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_contenido_materia1` FOREIGN KEY (`materia`) REFERENCES `materia` (`idmateria`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_contenido_usuario1` FOREIGN KEY (`user`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `curso`
 --
 ALTER TABLE `curso`
   ADD CONSTRAINT `fk_curso_materia1` FOREIGN KEY (`materia`) REFERENCES `materia` (`idmateria`) ON DELETE CASCADE,
-  ADD CONSTRAINT `fk_curso_notas1` FOREIGN KEY (`notas`) REFERENCES `notas` (`idnotas`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_curso_notas1` FOREIGN KEY (`notas`) REFERENCES `notas` (`idnotas`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_curso_usuario1` FOREIGN KEY (`estudiante`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_curso_usuario2` FOREIGN KEY (`profesor`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
@@ -432,7 +374,7 @@ ALTER TABLE `curso`
 -- Filtros para la tabla `notas`
 --
 ALTER TABLE `notas`
-  ADD CONSTRAINT `fk_notas_Proyecto1` FOREIGN KEY (`proyecto`) REFERENCES `proyecto` (`idProyecto`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_notas_Proyecto1` FOREIGN KEY (`proyecto`) REFERENCES `proyecto` (`idProyecto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
